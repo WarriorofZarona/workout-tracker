b661d3f72f89cd8a154e930e49bb696b47511f02
Andrew Bautista <andrewbautista85@gmail.com>
2020-04-17 15:00:16 -0400

    noted in comments the routes needed

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ db.on("error", error => {
 });
 
 // Code here
+// According to the api.js file, links need to go to /api/workouts/ for getLastWorkout
+// /api/workouts/id to addExercise
+// /api/workouts to createWorkout
+// /api/workouts/range to getWorkoutsInRange
 
 app.listen(3000, () => {
     console.log("App running on port 3000!");
