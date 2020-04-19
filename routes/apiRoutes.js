a12b12372f0d46ff262be77ad85ca55122a1b1ac
Andrew Bautista <andrewbautista85@gmail.com>
2020-04-19 15:50:43 -0400

    moved api comments from server.js to apiRoutes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,7 @@
+
+
+// Code here
+// According to the api.js file, links need to go to /api/workouts/ for getLastWorkout
+// /api/workouts/id to addExercise
+// /api/workouts to createWorkout
+// /api/workouts/range to getWorkoutsInRange
